a1ce019586b78fe91c644a552d1c5e8cf26b656e
MarcelL1989 <marcelludwig89@icloud.com>
2023-02-06 16:32:36 +0100

    Favoritenbild für Favoritenliste hinzugefügt und Favoriten als Überschrift hinzugefügt. FavoritenItem erstellt für das einzelne Card Viewobjekt Recyclerview mit Cardview erstellt und Adapter dafür gebaut (ZIP Datei wurde damals an Tutor versendet zum Helfen

---
+++ app/src/main/java/com/example/my_motivation/data/adapter/FavoriteAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.my_motivation.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.my_motivation.R
+import com.example.my_motivation.data.model.Detailcard
+
+class ItemAdapter(private val dataSet: List<Detailcard>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var text: TextView = itemView.findViewById(R.id.favourite_txt)
+        var iv: ImageView = itemView.findViewById(R.id.favourite_pic)
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        var listItem: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.favorititem, parent, false)
+        return ItemViewHolder(listItem)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        var currentFavourite = dataSet[position]
+        holder.text.text = currentFavourite.spruch
+        holder.iv.load(currentFavourite.bild)
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+
+    }
+
+
+}
