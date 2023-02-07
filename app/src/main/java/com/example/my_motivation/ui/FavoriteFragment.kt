e6e41ed6ef9a18000d0879b2f39db62fccd3693a
MarcelL1989 <marcelludwig89@icloud.com>
2023-02-07 15:51:34 +0100

    Schrift im Detailfragment besser besser lesbar im Kontrast gemacht, durch hinzufügen von viewCards. Favoritenbutton nun mit databinding aktiviert

---
+++ app/src/main/java/com/example/my_motivation/ui/FavoriteFragment.kt
@@ -0,0 +1,33 @@
+package com.example.my_motivation.ui
+
+import MainviewModel
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.example.my_motivation.data.adapter.CategoryCardAdapter
+import com.example.my_motivation.databinding.FragmentFavoritenBinding
+import com.example.my_motivation.databinding.FragmentHomeBinding
+
+class FavoriteFragment: Fragment() {
+    lateinit var binding: FragmentFavoritenBinding
+    val viewModel: MainviewModel by viewModels()
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFavoritenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
